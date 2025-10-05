360ca15b90594329a2e69bba097e386eaa2a2a9b
Irving Cazaban <irvingcazaban@mac-2.home>
2025-10-05 16:24:45 +0200

    mise en place de ma base de données

---
+++ src/Entity/Reservation.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $sieges_reserves = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?trajet $trajet = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?utilisateur $utilisateur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSiegesReserves(): ?int
+    {
+        return $this->sieges_reserves;
+    }
+
+    public function setSiegesReserves(int $sieges_reserves): static
+    {
+        $this->sieges_reserves = $sieges_reserves;
+
+        return $this;
+    }
+
+    public function getTrajet(): ?trajet
+    {
+        return $this->trajet;
+    }
+
+    public function setTrajet(?trajet $trajet): static
+    {
+        $this->trajet = $trajet;
+
+        return $this;
+    }
+
+    public function getUtilisateur(): ?utilisateur
+    {
+        return $this->utilisateur;
+    }
+
+    public function setUtilisateur(?utilisateur $utilisateur): static
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+}
